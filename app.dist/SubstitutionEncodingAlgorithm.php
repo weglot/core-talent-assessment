17637e99062b35002b95e1ec77597b2503d3f1aa
robin <robin@DESKTOP-AQI79D7.localdomain>
2020-02-07 10:15:33 +0100

    Started talent assessment

---
+++ app.dist/SubstitutionEncodingAlgorithm.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Class SubstitutionEncodingAlgorithm
+ *  http://www.writephponline.com/
+ */
+class SubstitutionEncodingAlgorithm implements EncodingAlgorithm
+{
+    /**
+     * @var array
+     */
+    private $substitutions;
+
+    /**
+     * SubstitutionEncodingAlgorithm constructor.
+     * @param $substitutions
+     */
+    public function __construct(array $substitutions)
+    {
+        $this->substitutions = $substitutions;
+    }
+
+    /**
+     * Encodes text by substituting character with another one provided in the pair.
+     * For example pair "ab" defines all "a" chars will be replaced with "b" and all "b" chars will be replaced with "a"
+     * Examples:
+     *      substitutions = ["ab"], input = "aabbcc", output = "bbaacc"
+     *      substitutions = ["ab", "cd"], input = "adam", output = "bcbm"
+     *
+     * @param string $text
+     * @return string
+     */
+    public function encode($text)
+    {
+        /**
+         * @TODO: Implement it
+         */
+
+        return '';
+    }
+}
