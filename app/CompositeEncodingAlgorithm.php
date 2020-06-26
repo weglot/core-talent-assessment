9b35386697a36116edda24d824577fa67e8f435c
Le Montagner <victorlemonta@gmail.com>
2020-06-26 06:52:47 +0200

    Removed the app file in gitignore .

---
+++ app/CompositeEncodingAlgorithm.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *  Class CompositeEncodingAlgorithm
+ */
+class CompositeEncodingAlgorithm implements EncodingAlgorithm
+{
+    /**
+     * @var EncodingAlgorithm[]
+     */
+    private $algorithms;
+
+    /**
+     *  CompositeEncodingAlgorithm constructor
+     */
+    public function __construct()
+    {
+        $this->algorithms = array();
+    }
+
+    /**
+     * @param EncodingAlgorithm $algorithm
+     */
+    public function add(EncodingAlgorithm $algorithm)
+    {
+        $this->algorithms[] = $algorithm;
+    }
+
+    /**
+     *  Encodes text using multiple Encoders stored in $this->algorithms (in orders encoders were added)
+     *
+     * @param string $text
+     * @return string
+     * http://www.writephponline.com/
+     */
+    public function encode($text)
+    {
+        /**
+         * @TODO: Implement it
+         */
+
+        return '';
+    }
+}
