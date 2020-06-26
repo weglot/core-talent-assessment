3d122fd17eeb24d910340f609153eb4b84ffa50e
Le Montagner <victorlemonta@gmail.com>
2020-06-26 07:52:06 +0200

    Implement encode in OffsetEncodingAlgorithm .

--- app/OffsetEncodingAlgorithm.php
+++ app/OffsetEncodingAlgorithm.php
@@ -38,8 +38,24 @@ class OffsetEncodingAlgorithm implements EncodingAlgorithm
         /**
          * @TODO: Implement it
          */
+        $newArray = array();
+        $stringToArray = $this->stringToArray($text);
+        $stringToArrayCharacters = $this->stringToArray(self::CHARACTERS);
+        foreach ($stringToArray as $string ){
+            $positionCaracter = strpos(self::CHARACTERS,$string);
 
-        return '';
+            if ( $positionCaracter === false){
+                array_push($newArray, $string);
+            } else {
+                $firstPart = array_slice($stringToArrayCharacters, $positionCaracter);
+                $secondPart = array_slice($stringToArrayCharacters, 0, $positionCaracter);
+                $arrayMerged = array_merge($firstPart, $secondPart);
+                array_push($newArray, $arrayMerged[$this->offset]);
+            }
+        }
+        $textEncoded = implode($newArray);
+
+        return $textEncoded;
     }
 
     public function stringToArray($s)
